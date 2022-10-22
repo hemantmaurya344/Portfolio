a1011dc5cd3948d7c9c52162d32fe15112d238ec
Hemant Maurya <hemantmaurya344@gmail.com>
2022-10-22 16:05:19 +0530

    added #1

---
+++ index.js
@@ -0,0 +1,13 @@
+
+function toHome(e){
+    const homeBtn = document.getElementById('home-page').scrollIntoView(true);    
+}
+function toabout(e){
+    const aboutBtn  = document.getElementById('about-page').scrollIntoView(true);
+}
+function toproject(e){
+    const projectBtn = document.getElementById('project-page').scrollIntoView(true); 
+}
+function tocontact(e){
+    const contactBtn = document.getElementById('contact-page').scrollIntoView(true);     
+}
